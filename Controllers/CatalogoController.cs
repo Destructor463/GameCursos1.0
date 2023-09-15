1c5aaecae101c49d3434be2ab9cbe6f70aaee19d
Panconk3so <cristopher_nieves@usmp.pe>
2023-09-14 19:49:02 -0500

    catalogo

---
+++ Controllers/CatalogoController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GameCursos1.0.Controllers
+{
+    [Route("[controller]")]
+    public class CatalogoController : Controller
+    {
+        private readonly ILogger<CatalogoController> _logger;
+
+        public CatalogoController(ILogger<CatalogoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
