00ee0572e23b78dca9bfe3e9028a7cac4745e5de
Destructor463 <alexingeniero2020@gmail.com>
2023-10-07 12:30:17 -0500

    Avance 7

---
+++ Models/Pago.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameCursos.Models
+{
+[Table("t_pago")]
+    public class Pago
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string? NombreTarjeta { get; set; }
+
+        public string? NumeroTarjeta { get; set; }
+
+        [NotMapped]
+        public string? DueDateYYMM { get; set; }
+        [NotMapped]
+        public string? Cvv { get; set; }
+        public Decimal MontoTotal{ get; set; }
+        public string? UserID{ get; set; }
+    }
+}
