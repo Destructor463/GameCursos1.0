35e114d04b33839217229d3d0a55043b673a7626
Destructor463 <alexingeniero2020@gmail.com>
2023-10-07 21:26:18 -0500

    Avance 22

---
+++ Controllers/PagoController.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using GameCursos.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using GameCursos.Models;
+
+namespace GameCursos.Controllers
+{
+    public class PagoController : Controller
+    {
+        private readonly ILogger<PagoController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;        
+
+        public PagoController(ILogger<PagoController> logger,
+            ApplicationDbContext context,
+            UserManager<IdentityUser> userManager )
+        {
+            _logger = logger;
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Create(Decimal monto)
+        {
+
+            Pago pago = new Pago();
+            pago.UserID = _userManager.GetUserName(User);
+            pago.MontoTotal = monto;
+            return View(pago);
+        }
+
+        [HttpPost]
+        public IActionResult Pagar(Pago pago)
+        {
+            pago.PaymentDate = DateTime.UtcNow;
+            _context.Add(pago);
+
+            var itemsCarrito = from o in _context.DataCarrito select o;
+            itemsCarrito = itemsCarrito.
+                Include(p => p.Producto).
+                Where(s => s.UserID.Equals(pago.UserID) && s.Status.Equals("PENDIENTE"));
+
+            Pedido pedido = new Pedido();
+            pedido.UserID = pago.UserID;
+            pedido.Total = pago.MontoTotal;
+            pedido.pago = pago;
+            pedido.Status = "PENDIENTE";
+            _context.Add(pedido);
+
+            List<DetallePedido> itemsPedido = new List<DetallePedido>();
+            foreach(var item in itemsCarrito.ToList()){
+                DetallePedido detallePedido = new DetallePedido();
+                detallePedido.pedido=pedido;
+                detallePedido.Precio = item.Precio;
+                detallePedido.Producto = item.Producto;
+                detallePedido.Cantidad = item.Cantidad;
+                itemsPedido.Add(detallePedido);
+            }
+
+
+            _context.AddRange(itemsPedido);
+
+            foreach (Carrito p in itemsCarrito.ToList())
+            {
+                p.Status="PROCESADO"; //ATENDIDA
+            }
+
+            _context.UpdateRange(itemsCarrito);
+
+            _context.SaveChanges();
+
+            ViewData["Message"] = "El pago se ha registrado y su pedido nro "+ pedido.ID +" esta en camino";
+            return View("Create");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
