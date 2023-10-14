7818f7c10f94a6b37c39e4dd57113c3fe803311f
Destructor463 <alexingeniero2020@gmail.com>
2023-10-13 21:54:52 -0500

    Avance 30

---
+++ Controllers/ProductoController.cs
@@ -0,0 +1,163 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GameCursos.Data;
+using GameCursos.Models;
+
+namespace GameCursos.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Producto
+        public async Task<IActionResult> Index()
+        {
+              return _context.DataProductos != null ? 
+                          View(await _context.DataProductos.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.DataProductos'  is null.");
+        }
+
+        // GET: Producto/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.DataProductos == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.DataProductos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+
+        // GET: Producto/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Producto/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Descripcion,Precio,PorcentajeDescuento,ImageName,Status")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(producto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producto);
+        }
+
+        // GET: Producto/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.DataProductos == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.DataProductos.FindAsync(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: Producto/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Descripcion,Precio,PorcentajeDescuento,ImageName,Status")] Producto producto)
+        {
+            if (id != producto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(producto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductoExists(producto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producto);
+        }
+
+        // GET: Producto/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.DataProductos == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.DataProductos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+
+        // POST: Producto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.DataProductos == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.DataProductos'  is null.");
+            }
+            var producto = await _context.DataProductos.FindAsync(id);
+            if (producto != null)
+            {
+                _context.DataProductos.Remove(producto);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductoExists(int id)
+        {
+          return (_context.DataProductos?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
